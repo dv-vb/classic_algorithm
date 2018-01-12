5a8b6b3f80c09a04e8825cd7814e7bd37a40b195
wujincheng <wujincheng@ruijie.com.cn>
2018-01-12 09:38:15 +0800

    modify(test1): the algorithm for find a number exist or not in a array.

--- test1.c
+++ test1.c
@@ -2,20 +2,46 @@
 
 #include <stdio.h>
 
+int find(int num, int *array, int rows, int cols)
+{
+    int row, col = 0, pos;
 
+    row = rows - 1;
+    /*
+     * 从最后一行第一列开始找，如果这个数比当前位置的大，意味着该数在当前列之后，所以列++
+     * 如果当前位置大于这个数，意味着该数在当前行之上，所以行--
+     */
+    while (row >= 0 && col < cols) {
+        pos = ((row) * (cols)) + col;
+        printf("%d ", array[pos]);
+        if (array[pos] == num) {
+            return 0;
+        } else if (array[pos] < num) {
+            ++col;
+        } else {
+            --row;
+        }
+    }
+
+    return -1;
+}
 
 int main(int argc, char const *argv[])
 {
     int array[10][7] = {0};
     int i, j;
+    int result;
 
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 7; j++) {
-            array[i][j] = i+j;
+            array[i][j] = i*j + i+j;
+            printf("%3d ", i*j + i+j);
         }
+        printf("\n");
     }
 
-    printf("row num: %p, %p, %ld\n", &array[0][1], &array[1][1], (&array[1][1]- &array[0][1]));
+    result = find(41, array, 10, 7);
+    printf("result: %d\n", result);
 
     return 0;
 }
