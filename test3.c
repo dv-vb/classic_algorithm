5509615bec81cfc726d2a77d40fa32f166ab7ce2
wujincheng <wujincheng@ruijie.com.cn>
2018-01-30 21:43:58 +0800

    refactor(test3): improve the performance.

--- test3.c
+++ test3.c
@@ -9,6 +9,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static bool check_different_basic(char *str)
 {
@@ -27,13 +28,53 @@ static bool check_different_basic(char *str)
     return true;
 }
 
+static bool check_diff_high_pri(char *str)
+{
+#define PER_LONG (sizeof(unsigned long) * 8)
+#define MAX      (3000 / PER_LONG)
+    unsigned long *bitmap, *p, index, bit;
+    int len, i;
+
+    len = strlen(str);
+    bitmap = (unsigned long *)malloc(MAX);
+    if (bitmap == NULL) {
+        return false;
+    }
+    memset(bitmap, 0, MAX);
+    for (i = 0; i < len; i++) {
+        index = (str[i] / PER_LONG);
+        bit = (str[i] % PER_LONG);
+        p = bitmap + index;
+#ifdef __DEBUG__
+        printf("bitmap: 0x%lx, index: %ld, str: %c, bit: %ld, 0x%lx\n", *p, index, str[i], bit, (1UL << bit));
+#endif
+        if ((*p & (1UL << bit))) {
+            free(bitmap);
+            return false;
+        }
+        *p |= (1UL << bit);
+    }
+    free(bitmap);
+
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
     char test[100] = {"qwertyuiop[]asdfghjklzxcvbnm,./1234567890"};
+    char test_pri[100] = {"qwertyuiop[]asdfghjkl;'zxcvbnm,./12345678990"};
     bool result;
 
     result = check_different_basic(test);
-    printf("result: %s, %s\n", test, result?"True":"False");
+    printf("result: %s, %s\n", test, result ? "True":"False");
+    result = check_diff_high_pri(test);
+    printf("high_pri: %s\n", result ? "True":"False");
+
+    result = check_different_basic(test_pri);
+    printf("result: %s, %s\n", test_pri, result ? "True":"False");
+    result = check_diff_high_pri(test_pri);
+    printf("high_pri: %s\n", result ? "True":"False");
+
 
     return 0;
 }
